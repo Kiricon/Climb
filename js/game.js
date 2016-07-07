d7c4876b73b4a1f7392eecb596eeab56dae9d1d6
kiricon <kiricon@live.com>
2016-07-06 19:16:06 -0600

    Added dead screen and fixed center alignment for score.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,11 @@
 function Game(){
   this.status = "setup";
   this.canvas = document.getElementById('canvas');
+  /*
+  this.canvas.height = window.innerHeight;
+  this.canvas.width = 3*(this.canvas.height/4);
+  this.canvas.style.height = this.canvas.height+"px";
+  this.canvas.style.width = this.canvas.width+"px"; */
   this.ctx = this.canvas.getContext('2d');
   this.sprite = new Square();
   this.walls = [];
@@ -34,12 +39,23 @@ Game.prototype.draw = function(){
       ctx.fillText("Press Space to Start", this.canvas.width/4, this.canvas.height/2);
       ctx.restore();
     }
+    if(this.status == "dead"){
+      ctx.save();
+      ctx.fillStyle = "black";
+      ctx.font="30px Arial";
+      var width = ctx.measureText("You died!").width;
+      ctx.fillText("You died!", this.canvas.width/2-width/2);
+      ctx.restore();
+    }
     // Draw score
     ctx.save();
     ctx.fillStyle= "black";
     ctx.font= "30px Arial";
-    var scoreWidth = ctx.measureText(this.highScore).width;
-    ctx.fillText(this.highScore, this.canvas.width/2-scoreWidth, y(2));
+    var hiscoreWidth = ctx.measureText(this.highScore).width;
+    ctx.fillText(this.highScore, this.canvas.width/2-hiscoreWidth/2, y(2));
+    ctx.font= "20px Arial";
+    var scoreWidth = ctx.measureText(sprite.score).width;
+    ctx.fillText(sprite.score, this.canvas.width/2-scoreWidth/2, y(3));
     ctx.restore();
 
   // DELETE OLD ONES
@@ -99,6 +115,11 @@ Game.prototype.listen = function(){
       self.status = "active";
     }
   }, false);
+  document.getElementsByTagName('body')[0].addEventListener('touchstart', function(e){
+    e.preventDefault();
+    self.sprite.jump();
+    self.status = "active";
+  })
 }
 
 Game.prototype.setupSprite = function(){
